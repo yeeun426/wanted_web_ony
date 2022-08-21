3096c3191eba3cfe633c8bb7acf84bb282d74744
yeeun426 <thsudkcla7@naver.com>
2022-08-22 04:40:24 +0900

    20220822 swiper 컴포넌트 구현

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import styled from 'styled-components';
 import Header from '../components/header';
+import Swiper from '../components/swiper';
 
 export default function IndexPage() {
   return (
     <div>
       <Header page="index" />
+      <Swiper />
       <div>컴공선배 리액트 템플릿 입니다.</div>
     </div>
   );
